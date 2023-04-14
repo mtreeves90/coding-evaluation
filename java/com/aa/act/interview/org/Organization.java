8e814fc88e447960660334417883094c1ecb52c3
mtreeves90 <41301049+mtreeves90@users.noreply.github.com>
2023-04-13 20:40:39 -0700

    Finished-Organization-Class
    Added code to hire new employees efficiently.

--- java/com/aa/act/interview/org/Organization.java
+++ java/com/aa/act/interview/org/Organization.java
@@ -1,13 +1,28 @@
 package com.aa.act.interview.org;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
+import java.util.Random;
+import java.util.Stack;
 
 public abstract class Organization {
 
+    private final static int RANDOM_ID_NUM_RANGE = 1000000;
+    
 	private Position root;
+    private Map<String, Stack<Position>> organizationMap;
 	
+    /*
+     * Add each position into the map to allow the retrievel of each position in constant time
+     * In this example, only one employee exists for each position, however, a stack is placed in the value for the map to accomodate for a position being open for multiple employees (Ex: multiple software engineers)
+     */
 	public Organization() {
 		root = createOrganization();
+        organizationMap = new HashMap<>();
+        initiatePositions(root);
 	}
 	
 	protected abstract Position createOrganization();
@@ -20,8 +35,22 @@ public abstract class Organization {
 	 * @return the newly filled position or empty if no position has that title
 	 */
 	public Optional<Position> hire(Name person, String title) {
-		//your code here
-		return Optional.empty();
+        Optional<Position> optionalPosition = Optional.empty();
+        Stack<Position> positionStack = organizationMap.get(title);
+        
+        //Pop from the stack for position if position is open to hire employee
+        if (positionStack.peek() != null) {
+            optionalPosition = Optional.of(positionStack.pop());
+        }
+        
+        if (optionalPosition.isPresent()) {
+            Position position = optionalPosition.get();
+            //Generate random ID number for new employee
+            Random random = new Random();
+            position.setEmployee(Optional.of(new Employee(random.nextInt(RANDOM_ID_NUM_RANGE), person)));
+        }
+        
+		return optionalPosition;
 	}
 
 	@Override
@@ -36,4 +65,20 @@ public abstract class Organization {
 		}
 		return sb.toString();
 	}
+    
+    private void initiatePositions(Position position) {
+        Stack<Position> currentStack;
+        String title = position.getTitle();
+        currentStack = organizationMap.getOrDefault(title, new Stack<>());
+        
+        currentStack.push(position);
+        organizationMap.put(title, currentStack);
+                                   
+        Collection<Position> directReports = position.getDirectReports();
+        
+        //Depth first search to specify each position
+        for (Position directReport : directReports) {
+            initiatePositions(directReport);
+        }
+    }
 }
